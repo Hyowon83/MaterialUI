3d2fb9b5b042ec73ab1701cc988470d6b361be8a
Hyowon83 <aile1383@gmail.com>
2021-12-27 17:05:43 +0900

    02. 화면 구성 틀

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
+import WebHard from './components/WebHard';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route exact path="/" element={<SignIn />} />
+          <Route exact path="/" element={<WebHard />} />
           <Route path="/signUp" element={<SignUp />} />
         </Routes>
       </div>
